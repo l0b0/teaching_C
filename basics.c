52b00d9d10ecfac661599956f2d8778aed400013
Victor Engmark <victor.engmark@gmail.com>
2013-02-01 11:51:47 +0100

    Introduction
    From
    <http://www-h.eng.cam.ac.uk/help/tpl/languages/C/teaching_C/node3.html>.

---
+++ basics.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mean(int a,int b)
+{
+  return (a + b)/2;
+}
+
+int main()
+{
+int i, j;
+int answer;
+   /* comments are done like this */
+   i = 7;
+   j = 9;
+
+   answer = mean(i,j);
+   printf("The mean of %d and %d is %d\n", i, j, answer);
+   exit (0);
+}
